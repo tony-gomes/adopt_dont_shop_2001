c13b82650145143265b9175ab29d1c6873cdae01
Anthony Gomes <code@tonygom.es>
2020-03-18 21:22:51 -0600

    Add welcome test

---
+++ spec/features/welcome/user_can_see_welcome_page_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "roots to welcome page w/ links", type: :feature do
+  it "can see welcome#index" do
+
+    visit "/"
+
+    page.has_css?(".welcome_message")
+    expect(page).to have_link("All Shelters", href: "/shelters")
+    expect(page).to have_link("Add New Shelter", href: "/shelters/new")
+  end
+end
