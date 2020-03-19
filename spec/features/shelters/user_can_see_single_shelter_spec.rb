0ee64af9ca8b19449c718b9dee7c9c865cc97c44
Anthony Gomes <code@tonygom.es>
2020-03-18 22:42:48 -0600

    Update single shelter test

---
+++ spec/features/shelters/user_can_see_single_shelter_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "single shelter page", type: :feature do
+  it "can see single shelter name, address, city, state, zip" do
+    shelter_1 = Shelter.create(name: "Doggie Dog World",
+                               address: "123 Main St.",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80202")
+    shelter_2 = Shelter.create(name: "Doggie Day",
+                               address: "987 Spring Lake",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80212")
+    shelter_3 = Shelter.create(name: "Dog Lovers",
+                               address: "456 Rocky Road",
+                               city: "Lala Land",
+                               state: "North Nowhere",
+                               zip: "90210")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+    expect(page).to have_no_content(shelter_2.name)
+    expect(page).to have_no_content(shelter_3.name)
+  end
+end
