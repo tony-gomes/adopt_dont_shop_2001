08eb509a1de3824116d4179e34830addf14e61b6
Anthony Gomes <code@tonygom.es>
2020-03-21 09:00:55 -0600

    Add validation for pet fields w/o image check

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,8 @@
 class Pet < ApplicationRecord
+  validates_presence_of :name,
+                        :description,
+                        :approximate_age,
+                        :sex,
+                        :adoption_status,
+                        :current_shelter_id
 end
